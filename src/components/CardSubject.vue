53c33513de3b5ee1171e6c8f531b637fdb8a150d
Andara <andarasophan@gmail.com>
2020-03-20 15:21:51 +0700

    template for subject-page

---
+++ src/components/CardSubject.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-col>
+    <v-card
+      class="mx-auto"
+      max-width="350"
+    >
+      <v-img
+        height="250"
+        :src="img"
+      ></v-img>
+
+      <v-card-title>{{ title }}</v-card-title>
+
+      <v-card-text>
+        <div>Chapter2nya</div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn
+          color="deep-purple lighten-2"
+          text
+        >
+          Start
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    img: String
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Subject.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row>
+      <CardSubject
+        v-for="subject in subjects"
+        :key="subject.id"
+        :title="subject.title"
+        :img="subject.img"
+      />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import CardSubject from '../components/CardSubject'
+
+export default {
+  name: 'Subject',
+  components: {
+    CardSubject
+  },
+  computed: {
+    subjects () {
+      return this.$store.state.subjects
+    }
+  },
+  created () {
+    this.$store.dispatch('fetchListOfSubjects')
+  }
+}
+</script>
+
+<style>
+
+</style>

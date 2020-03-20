0d119a56e040625cdfcb7d8bbcf736c824347f60
Andara <andarasophan@gmail.com>
2020-03-20 17:38:08 +0700

    add filter by level

--- src/views/Subjects/Subjects.vue
+++ src/views/Subjects/Subjects.vue
@@ -1,5 +1,14 @@
 <template>
   <v-container>
+    <v-row align="center">
+        <v-col class="d-flex" cols="12" sm="4">
+          <v-select
+            v-model="select"
+            :items="items"
+            label="Level"
+          ></v-select>
+        </v-col>
+    </v-row>
     <v-row>
       <CardSubject
         v-for="subject in subjects"
@@ -16,11 +25,20 @@ import CardSubject from './components/CardSubject'
 
 export default {
   name: 'Subject',
+  data () {
+    return {
+      select: 'All',
+      items: ['All', 1, 2, 3]
+    }
+  },
   components: {
     CardSubject
   },
   computed: {
     subjects () {
+      if (this.select !== 'All') {
+        return this.$store.state.subjects.subjects.filter(subject => subject.level === this.select)
+      }
       return this.$store.state.subjects.subjects
     }
   },
